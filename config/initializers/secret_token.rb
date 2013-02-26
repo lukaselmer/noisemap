267e8bef45750c3c3dab254a71adcfa2fdb10849
Lukas Elmer <lukas.elmer@gmail.com>
2013-02-26 16:13:29 +0100

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Noisemap::Application.config.secret_key_base = 'ecdf06ca3d16d7ddfab06e871e55aa674537e3ada1ffffe7c8fceae1aea9defc83f859c1dcd9410e5083a97f9aa9fc4bff1e0ce20260f23cdcf054609ef25293'
